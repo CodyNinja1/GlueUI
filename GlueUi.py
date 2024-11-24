08e92cd3d8fe11185aa944d998dd56c6c8ded144
JailMan <jailmanguy1@gmail.com>
2024-11-24 12:23:12 +0300

    Add GlueUi license

--- GlueUi.py
+++ GlueUi.py
@@ -1,3 +1,19 @@
+# GlueUi: A basic UI library built upon pySDL2 for Python 3.12
+# Copyright (C) 2024 Ahmad S.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from typing import Literal
 import sdl2
 import sdl2.ext as sdl2ext
